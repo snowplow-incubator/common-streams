e0e6be23bce8797cfee5cfbe6a815861e65336f4
Ian Streeter <ian@snowplowanalytics.com>
2023-10-20 13:20:08 +0100

    Publish independent runtime-common module

--- modules/runtime-common/src/main/scala/com/snowplowanalytics/snowplow/runtime/HealthProbe.scala
+++ modules/runtime-common/src/main/scala/com/snowplowanalytics/snowplow/runtime/HealthProbe.scala
@@ -5,7 +5,7 @@
  * and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
  * You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
  */
-package com.snowplowanalytics.snowplow.loaders.runtime
+package com.snowplowanalytics.snowplow.runtime
 
 import cats.effect.{Async, Resource, Sync}
 import cats.data.Kleisli
