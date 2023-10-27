9d1285026681b9a23625d87688e095ca62693b32
colmsnowplow <colm@snowplowanalytics.com>
2023-10-27 12:37:59 +0100

    Add Kinesis Sink tests

--- modules/kinesis-it/src/test/scala/com/snowplowanalytics/snowplow/sources/kinesis/Utils.scala
+++ modules/kinesis-it/src/test/scala/com/snowplowanalytics/snowplow/sources/kinesis/Utils.scala
@@ -5,23 +5,31 @@
  * and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
  * You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
  */
-package com.snowplowanalytics.snowplow.sources.kinesis
+package com.snowplowanalytics.snowplow.it.kinesis
 
 import cats.effect.{IO, Ref}
+
+import scala.concurrent.duration.FiniteDuration
+import scala.jdk.CollectionConverters._
 
 import eu.timepit.refined.types.numeric.PosInt
 
 import software.amazon.awssdk.core.SdkBytes
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
-import software.amazon.awssdk.services.kinesis.model.{PutRecordRequest, PutRecordResponse}
+import software.amazon.awssdk.services.kinesis.model.{GetRecordsRequest, GetShardIteratorRequest, PutRecordRequest, PutRecordResponse}
+
+import com.snowplowanalytics.snowplow.sources.{EventProcessor, TokenedEvents}
+import com.snowplowanalytics.snowplow.sources.kinesis.KinesisSourceConfig
+import com.snowplowanalytics.snowplow.sinks.kinesis.{BackoffPolicy, KinesisSinkConfig}
 
 import java.net.URI
 import java.nio.charset.StandardCharsets
 import java.util.UUID
 import java.time.Instant
+import java.util.concurrent.TimeUnit
 
-import com.snowplowanalytics.snowplow.sources.{EventProcessor, TokenedEvents}
+import software.amazon.awssdk.services.kinesis.model.DescribeStreamRequest
 
 object Utils {
 
@@ -42,7 +50,44 @@ object Utils {
     IO.blocking(client.putRecord(record).get())
   }
 
-  def getKinesisConfig(endpoint: URI)(streamName: String): KinesisSourceConfig = KinesisSourceConfig(
+  /**
+   * getDataFromKinesis gets the last 1000 records from kinesis, stringifies the datta it found, and
+   * returns a ReceivedEvents It can be called at the end of simple tests to return data from a
+   * Kinesis stream.
+   *
+   * If required in future, where more data is used we might amend it to poll the stream for data
+   * and return everything it finds after a period without any data.
+   */
+  def getDataFromKinesis(
+    client: KinesisAsyncClient,
+    streamName: String
+  ): ReceivedEvents = {
+
+    val descStreamResp = client.describeStream(DescribeStreamRequest.builder().streamName(streamName).build()).get
+
+    // We're assuming only one shard here.
+    // Any future test with multiple shards requires us to create one iterator per shard
+    val shIterRequest = GetShardIteratorRequest
+      .builder()
+      .streamName(streamName)
+      .shardIteratorType("TRIM_HORIZON")
+      .shardId(descStreamResp.streamDescription.shards.get(0).shardId)
+      .build();
+
+    val shIter = client.getShardIterator(shIterRequest).get.shardIterator
+
+    val request = GetRecordsRequest
+      .builder()
+      .streamARN(descStreamResp.streamDescription().streamARN())
+      .shardIterator(shIter)
+      .build()
+
+    val out =
+      ReceivedEvents(client.getRecords(request).get().records().asScala.toList.map(record => new String(record.data.asByteArray())), None)
+    out
+  }
+
+  def getKinesisSourceConfig(endpoint: URI)(streamName: String): KinesisSourceConfig = KinesisSourceConfig(
     UUID.randomUUID().toString,
     streamName,
     KinesisSourceConfig.InitialPosition.TrimHorizon,
@@ -50,6 +95,14 @@ object Utils {
     PosInt.unsafeFrom(1),
     Some(endpoint),
     Some(endpoint),
+    Some(endpoint)
+  )
+
+  def getKinesisSinkConfig(endpoint: URI)(streamName: String): KinesisSinkConfig = KinesisSinkConfig(
+    streamName,
+    BackoffPolicy(FiniteDuration(1, TimeUnit.SECONDS), FiniteDuration(1, TimeUnit.SECONDS), None),
+    1000,
+    1000000,
     Some(endpoint)
   )
 
