c0aa9c17442012711491b8c4dc2a5ad33cf92f4f
Ian Streeter <ian@snowplowanalytics.com>
2023-11-25 19:48:05 +0000

    Reduce thread context switching in pubsub source (close #36)

--- modules/pubsub/src/main/scala/com/snowplowanalytics/snowplow/sources/pubsub/PubsubSource.scala
+++ modules/pubsub/src/main/scala/com/snowplowanalytics/snowplow/sources/pubsub/PubsubSource.scala
@@ -7,11 +7,8 @@
  */
 package com.snowplowanalytics.snowplow.sources.pubsub
 
-import cats.Monad
-import cats.effect.{Async, Sync}
+import cats.effect.{Async, Sync, Resource}
 import cats.effect.implicits._
-import cats.effect.kernel.{Deferred, DeferredSink, DeferredSource}
-import cats.effect.std._
 import cats.implicits._
 import fs2.{Chunk, Stream}
 import org.typelevel.log4cats.{Logger, SelfAwareStructuredLogger}
@@ -35,7 +32,8 @@ import com.snowplowanalytics.snowplow.sources.internal.{Checkpointer, LowLevelEv
 
 import scala.concurrent.duration.FiniteDuration
 
-import java.util.concurrent.{Callable, ScheduledExecutorService, ScheduledFuture, ScheduledThreadPoolExecutor, TimeUnit}
+import java.util.concurrent.{Callable, Phaser, ScheduledExecutorService, ScheduledFuture, ScheduledThreadPoolExecutor, Semaphore, TimeUnit}
+import java.util.concurrent.atomic.AtomicReference
 
 object PubsubSource {
 
@@ -70,42 +68,93 @@ object PubsubSource {
       }
   }
 
-  private case class SingleMessage[F[_]](
+  private case class SingleMessage(
     message: ByteBuffer,
     ackReply: AckReplyConsumer,
     tstamp: Instant
   )
 
-  private def pubsubStream[F[_]: Async](config: PubsubSourceConfig): Stream[F, LowLevelEvents[Chunk[AckReplyConsumer]]] = {
-    val resources = for {
-      dispatcher <- Stream.resource(Dispatcher.sequential(await = false))
-      queue <- Stream.eval(Queue.unbounded[F, SingleMessage[F]])
-      semaphore <- Stream.eval(Semaphore[F](config.bufferMaxBytes))
-      sig <- Stream.eval(Deferred[F, Either[Throwable, Unit]])
-      _ <- runSubscriber(config, queue, dispatcher, semaphore, sig)
-      _ <- Stream.bracket(Sync[F].unit)(_ => sig.complete(Right(())) *> Sync[F].cede)
-    } yield (queue, semaphore, sig)
+  /**
+   * The toolkit for coordinating concurrent operations between the message receiver and the fs2
+   * stream. This is the interface between FS2 and non-FS2 worlds.
+   *
+   * We use pure Java (non-cats-effect) classes so we can use them in the message receiver without
+   * needing to use a cats-effect Dispatcher.
+   *
+   * @param queue
+   *   A List of messages that have been provided by the Pubsub Subscriber. The FS2 stream should
+   *   periodically drain this queue.
+   * @param semaphore
+   *   A Semaphore used to manage how many bytes are being held in memory. When the queue holds too
+   *   many bytes, the semaphore will block on acquiring more permits. This is needed because we
+   *   have turned off FlowControl in the subscriber.
+   * @param phaser
+   *   A Phaser that advances each time the queue has new messages to be consumed. The FS2 stream
+   *   can wait on the phaser instead of repeatedly pooling the queue.
+   * @param errorRef
+   *   Any error provided to us by the pubsub Subscriber. If the FS2 stream sees an error here, then
+   *   it should raise the error.
+   */
+  private case class Control(
+    queue: AtomicReference[List[SingleMessage]],
+    semaphore: Semaphore,
+    phaser: Phaser,
+    errorRef: AtomicReference[Option[Throwable]]
+  )
 
-    resources.flatMap { case (queue, semaphore, sig) =>
-      Stream
-        .fromQueueUnterminated(queue)
-        .chunks
-        .filter(_.nonEmpty)
-        .map { chunk =>
-          val events         = chunk.map(_.message)
-          val acks           = chunk.map(_.ackReply)
-          val earliestTstamp = chunk.map(_.tstamp).iterator.min
-          LowLevelEvents(events, acks, Some(earliestTstamp))
-        }
-        .evalTap { case LowLevelEvents(events, _, _) =>
-          val numPermits = events.foldLeft(0L) { case (numPermits, e) =>
-            numPermits + permitsFor(config, e.limit())
-          }
-          semaphore.releaseN(numPermits)
-        }
-        .interruptWhen(sig)
+  private object Control {
+    def build[F[_]: Sync](config: PubsubSourceConfig): F[Control] = Sync[F].delay {
+      Control(
+        new AtomicReference(Nil),
+        new Semaphore(config.bufferMaxBytes, false),
+        new Phaser(2),
+        new AtomicReference(None)
+      )
     }
   }
+
+  private def pubsubStream[F[_]: Async](config: PubsubSourceConfig): Stream[F, LowLevelEvents[Chunk[AckReplyConsumer]]] =
+    for {
+      control <- Stream.eval(Control.build(config))
+      _ <- Stream.resource(runSubscriber(config, control))
+      events <- consumeFromQueue(config, control)
+    } yield events
+
+  private def consumeFromQueue[F[_]: Sync](
+    config: PubsubSourceConfig,
+    control: Control
+  ): Stream[F, LowLevelEvents[Chunk[AckReplyConsumer]]] =
+    Stream
+      .repeatEval {
+        Sync[F].delay(control.errorRef.get).flatMap {
+          case None            => Sync[F].unit
+          case Some(throwable) => Sync[F].raiseError[Unit](throwable)
+        }
+      }
+      .evalMap { _ =>
+        // Semantically block until message receive has written at least one message
+        val waitForData = Sync[F].interruptible {
+          control.phaser.awaitAdvanceInterruptibly(control.phaser.arrive())
+        }
+        Sync[F].uncancelable { poll =>
+          poll(waitForData) *> Sync[F].delay(control.queue.getAndSet(Nil))
+        }
+      }
+      .filter(_.nonEmpty) // Would happen if phaser was terminated
+      .map { list =>
+        val events         = Chunk.iterator(list.iterator.map(_.message))
+        val acks           = Chunk.iterator(list.iterator.map(_.ackReply))
+        val earliestTstamp = list.iterator.map(_.tstamp).min
+        LowLevelEvents(events, acks, Some(earliestTstamp))
+      }
+      .evalTap { case LowLevelEvents(events, _, _) =>
+        val numPermits = events.foldLeft(0) { case (numPermits, e) =>
+          numPermits + permitsFor(config, e.remaining())
+        }
+        Sync[F].delay {
+          control.semaphore.release(numPermits)
+        }
+      }
 
   /**
    * Number of semaphore permits needed to write an event to the buffer.
@@ -114,31 +163,23 @@ object PubsubSource {
    *   - For large events, there are not enough permits available for the event in bytes, so return
    *     the number of available permits.
    */
-  private def permitsFor(config: PubsubSourceConfig, bytes: Int): Long =
-    Math.min(config.bufferMaxBytes, bytes.toLong)
+  private def permitsFor(config: PubsubSourceConfig, bytes: Int): Int =
+    Math.min(config.bufferMaxBytes, bytes)
 
-  private def errorListener[F[_]: Sync](dispatcher: Dispatcher[F], sig: DeferredSink[F, Either[Throwable, Unit]]): ApiService.Listener =
+  private def errorListener(phaser: Phaser, errorRef: AtomicReference[Option[Throwable]]): ApiService.Listener =
     new ApiService.Listener {
-      override def failed(from: ApiService.State, failure: Throwable): Unit =
-        dispatcher.unsafeRunSync {
-          Logger[F].error(failure)("Error from Pubsub subscriber") *>
-            sig.complete(Left(failure)).void
-        }
+      override def failed(from: ApiService.State, failure: Throwable): Unit = {
+        errorRef.compareAndSet(None, Some(failure))
+        phaser.forceTermination()
+      }
     }
 
-  private def runSubscriber[F[_]: Async](
-    config: PubsubSourceConfig,
-    queue: Queue[F, SingleMessage[F]],
-    dispatcher: Dispatcher[F],
-    semaphore: Semaphore[F],
-    sig: Deferred[F, Either[Throwable, Unit]]
-  ): Stream[F, Unit] = {
-    val name     = ProjectSubscriptionName.of(config.subscription.projectId, config.subscription.subscriptionId)
-    val receiver = messageReceiver(config, queue, dispatcher, semaphore, sig)
-
+  private def runSubscriber[F[_]: Async](config: PubsubSourceConfig, control: Control): Resource[F, Unit] =
     for {
-      executor <- Stream.bracket(Sync[F].delay(scheduledExecutorService))(s => Sync[F].delay(s.shutdown()))
-      subscriber <- Stream.eval(Sync[F].delay {
+      executor <- Resource.make(Sync[F].delay(scheduledExecutorService))(s => Sync[F].delay(s.shutdown()))
+      receiver = messageReceiver(config, control)
+      name     = ProjectSubscriptionName.of(config.subscription.projectId, config.subscription.subscriptionId)
+      subscriber <- Resource.eval(Sync[F].delay {
                       Subscriber
                         .newBuilder(name, receiver)
                         .setMaxAckExtensionPeriod(convertDuration(config.maxAckExtensionPeriod))
@@ -157,59 +198,51 @@ object PubsubSource {
                         }
                         .build
                     })
-      _ <- Stream.eval(Sync[F].delay {
-             subscriber.addListener(errorListener(dispatcher, sig), MoreExecutors.directExecutor)
+      _ <- Resource.eval(Sync[F].delay {
+             subscriber.addListener(
+               errorListener(control.phaser, control.errorRef),
+               MoreExecutors.directExecutor // TODO: use the non-blocking executor for errors?
+             )
            })
-      _ <- Stream.bracket(Sync[F].delay(subscriber.startAsync())) { apiService =>
-             for {
-               _ <- Logger[F].info("Stopping the PubSub Subscriber...")
-               _ <- Sync[F].delay(apiService.stopAsync())
-               _ <- drainQueue(queue)
-               _ <- Logger[F].info("Waiing for the PubSub Subscriber to finish cleanly...")
-               _ <- Sync[F].blocking(apiService.awaitTerminated())
-             } yield ()
-           }
+      apiService <- Resource.make(Sync[F].delay(subscriber.startAsync())) { apiService =>
+                      for {
+                        _ <- Logger[F].info("Stopping the PubSub Subscriber...")
+                        _ <- Sync[F].delay(apiService.stopAsync())
+                        fiber <- drainQueue(control).start
+                        _ <- Logger[F].info("Waiting for the PubSub Subscriber to finish cleanly...")
+                        _ <- Sync[F].blocking(apiService.awaitTerminated(config.shutdownTimeout.toMillis, TimeUnit.MILLISECONDS))
+                        _ <- Sync[F].delay(control.phaser.forceTermination())
+                        _ <- fiber.join
+                      } yield ()
+                    }
+      _ <- Resource.eval(Sync[F].blocking(apiService.awaitRunning()))
     } yield ()
-  }
 
-  private def drainQueue[F[_]: Async](queue: QueueSource[F, SingleMessage[F]]): F[Unit] = {
-
-    def go(acc: List[AckReplyConsumer], queue: QueueSource[F, SingleMessage[F]]): F[List[AckReplyConsumer]] =
-      queue.tryTake.flatMap {
-        case Some(SingleMessage(_, acker, _)) =>
-          go(acker :: acc, queue)
-        case None =>
-          Monad[F].pure(acc)
-      }
-
-    go(Nil, queue).flatMap { ackers =>
-      pubsubCheckpointer.nack(Chunk.from(ackers))
+  private def drainQueue[F[_]: Async](control: Control): F[Unit] =
+    Async[F].untilDefinedM {
+      for {
+        _ <- Sync[F].delay(control.semaphore.release(Int.MaxValue - control.semaphore.availablePermits()))
+        phase <- Sync[F].blocking(control.phaser.arriveAndAwaitAdvance())
+        messages <- Sync[F].delay(control.queue.getAndSet(Nil))
+        _ <- pubsubCheckpointer.nack(Chunk.from(messages.map(_.ackReply)))
+      } yield if (phase < 0) None else Some(())
     }
-  }
 
-  private def messageReceiver[F[_]: Async](
+  private def messageReceiver(
     config: PubsubSourceConfig,
-    queue: QueueSink[F, SingleMessage[F]],
-    dispatcher: Dispatcher[F],
-    semaphore: Semaphore[F],
-    sig: DeferredSource[F, Either[Throwable, Unit]]
+    control: Control
   ): MessageReceiver =
     new MessageReceiver {
       def receiveMessage(message: PubsubMessage, ackReply: AckReplyConsumer): Unit = {
-        val tstamp = Instant.ofEpochSecond(message.getPublishTime.getSeconds, message.getPublishTime.getNanos.toLong)
-        val put = semaphore.acquireN(permitsFor(config, message.getData.size)) *>
-          queue.offer(SingleMessage(message.getData.asReadOnlyByteBuffer(), ackReply, tstamp))
-
-        val io = put
-          .race(sig.get)
-          .flatMap {
-            case Right(_) =>
-              Sync[F].delay(ackReply.nack())
-            case Left(_) =>
-              Sync[F].unit
-          }
-
-        dispatcher.unsafeRunSync(io)
+        val tstamp          = Instant.ofEpochSecond(message.getPublishTime.getSeconds, message.getPublishTime.getNanos.toLong)
+        val singleMessage   = SingleMessage(message.getData.asReadOnlyByteBuffer(), ackReply, tstamp)
+        val permitsRequired = permitsFor(config, singleMessage.message.remaining())
+        control.semaphore.acquire(permitsRequired)
+        val previousQueue = control.queue.getAndUpdate(list => singleMessage :: list)
+        if (previousQueue.isEmpty) {
+          control.phaser.arrive()
+        }
+        ()
       }
     }
 
