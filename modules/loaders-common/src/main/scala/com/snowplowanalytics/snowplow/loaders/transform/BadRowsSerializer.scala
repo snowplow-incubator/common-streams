cc9a7365ea4a815ad6796511a9f88e0ef3d47b50
Piotr Poniedziałek <pondzix@gmail.com>
2024-01-01 11:18:24 +0100

    Add badrows serializer checking max size (close #49)

---
+++ modules/loaders-common/src/main/scala/com/snowplowanalytics/snowplow/loaders/transform/BadRowsSerializer.scala
@@ -0,0 +1,35 @@
+package com.snowplowanalytics.snowplow.loaders.transform
+
+import com.snowplowanalytics.snowplow.badrows.{BadRow, Failure, Payload, Processor}
+
+import java.nio.charset.StandardCharsets
+import java.time.Instant
+
+object BadRowsSerializer {
+
+  /**
+   * If input bad row exceeds provided max size in bytes, return serialized SizeViolation bad row
+   * with trimmed original payload. If not, return original serialized bad row.
+   */
+  def withMaxSize(
+    badRow: BadRow,
+    processor: Processor,
+    maxSize: Int
+  ): Array[Byte] = {
+    val originalSerialized = badRow.compactByteArray
+    val originalSizeBytes  = originalSerialized.length
+
+    if (originalSizeBytes >= maxSize) {
+      val trimmedPayload = new String(originalSerialized, 0, maxSize / 10, StandardCharsets.UTF_8)
+      BadRow
+        .SizeViolation(
+          processor,
+          Failure.SizeViolation(Instant.now(), maxSize, originalSizeBytes, "Badrow exceeds allowed max size"),
+          Payload.RawPayload(trimmedPayload)
+        )
+        .compactByteArray
+    } else {
+      originalSerialized
+    }
+  }
+}
