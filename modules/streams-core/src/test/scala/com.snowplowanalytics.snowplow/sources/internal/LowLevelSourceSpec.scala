snowplow-incubator/common-streams

commit d54572177623bf2d8f25be5b1b1bc5ff34948046

Improve timings of overlapping windows (#64)
In Lake Loader, there is an edge-case scenario where the time taken to
"commit" events to the Lake becomes longer than the duration of the
configured timed window.

Previously, because of how windows got timed, the edge case scenario caused the
window sizes to get smaller and smaller as it waited for the events to be
committed.

This PR changes how a window length is timed. After this PR, the processing
time of a window remains constant, even if the "commit" phase starts to
exceed the window size.  It allows the loader to stay healthier even
during edge-case scenarios.

modules/streams-core/src/main/scala/com.snowplowanalytics.snowplow/sources/internal/LowLevelSource.scala
@@ -241,21 +241,20 @@ private[sources] object LowLevelSource {
             case Some(q) => Pull.eval(q.offer(None)) >> Pull.done
           }
         case Some((Left(_), next)) =>
-          val openWindow =
-            Pull.eval(Logger[F].info(s"Opening new window with duration ${config.duration}")) >> next.timeout(config.duration)
           current match {
-            case None    => openWindow >> go(next, None)
-            case Some(q) => openWindow >> Pull.eval(q.offer(None)) >> go(next, None)
+            case None    => go(next, None)
+            case Some(q) => Pull.eval(q.offer(None)) >> go(next, None)
           }
         case Some((Right(chunk), next)) =>
           current match {
             case None =>
-              for {
+              val pull = for {
                 q <- Pull.eval(Queue.synchronous[F, Option[A]])
                 _ <- Pull.output1(Stream.fromQueueNoneTerminated(q))
                 _ <- Pull.eval(chunk.traverse(a => q.offer(Some(a))))
-                _ <- go(next, Some(q))
-              } yield ()
+                _ <- Pull.eval(Logger[F].info(s"Opening new window with duration ${config.duration}")) >> next.timeout(config.duration)
+              } yield go(next, Some(q))
+              pull.flatten
             case Some(q) =>
               Pull.eval(chunk.traverse(a => q.offer(Some(a)))) >> go(next, Some(q))
           }
@@ -265,11 +264,11 @@ private[sources] object LowLevelSource {
       in.pull
         .timed { timedPull: Pull.Timed[F, A] =>
           val timeout = timeoutForFirstWindow(config)
-          for {
+          val pull = for {
             _ <- Pull.eval(Logger[F].info(s"Opening first window with randomly adjusted duration of $timeout"))
             _ <- timedPull.timeout(timeout)
-            _ <- go(timedPull, None)
-          } yield ()
+          } yield go(timedPull, None)
+          pull.flatten
         }
         .stream
         .prefetch // This prefetch is required to pull items into the emitted stream

commit 531dcfbdb48d6ff6ce05dbcee8d34a1bdf9299f8

Fix randomly adjusted first window size
common-streams has a feature where the first window is randomly adjusted
to a different size. This helps in a deployment where a large number of
pods might all start at the same time, but we pods to have mutually
staggered windows, e.g. to avoid write conflicts in the lake loader.

Unfortunately I broke the feature in #64 so this fixes it again.

I also add a test so it won't get broken again.

modules/streams-core/src/main/scala/com.snowplowanalytics.snowplow/sources/internal/LowLevelSource.scala
@@ -272,7 +272,9 @@ private[sources] object LowLevelSource {
           val pull = for {
             _ <- Pull.eval(Logger[F].info(s"Opening first window with randomly adjusted duration of $timeout"))
             _ <- timedPull.timeout(timeout)
-          } yield go(timedPull, None)
+            q <- Pull.eval(Queue.synchronous[F, Option[A]])
+            _ <- Pull.output1(Stream.fromQueueNoneTerminated(q))
+          } yield go(timedPull, Some(q))
           pull.flatten
         }
         .stream

modules/streams-core/src/test/scala/com.snowplowanalytics.snowplow/sources/internal/LowLevelSourceSpec.scala
@@ -39,6 +39,7 @@ class LowLevelSourceSpec extends Specification with CatsEffect {
       cleanly checkpoint pending window when a stream is interrupted $windowed2
       not checkpoint events if the event processor throws an exception $windowed3
       eagerly start windows when previous window is still finalizing $windowed4
+      use a short first window according to the configuration $windowed5
 
     When reporting healthy status
       report healthy when there are no events $health1
@@ -433,6 +434,57 @@ class LowLevelSourceSpec extends Specification with CatsEffect {
         Action.Checkpointed(List("17", "18", "19", "20")),
         Action.Checkpointed(List("21", "22", "23", "24")),
         Action.Checkpointed(List("25", "26"))
+      )
+    )
+
+    TestControl.executeEmbed(io)
+  }
+
+  def windowed5 = {
+
+    val config = EventProcessingConfig(
+      EventProcessingConfig.TimedWindows(
+        duration           = 60.seconds,
+        firstWindowScaling = 0.25, // so first window is 15 seconds
+        numEagerWindows    = 2
+      )
+    )
+
+    val testConfig = TestSourceConfig(
+      batchesPerRebalance = Int.MaxValue,
+      eventsPerBatch      = 2,
+      timeBetweenBatches  = 11.seconds,
+      timeToProcessBatch  = 1.second
+    )
+
+    val io = for {
+      refActions <- Ref[IO].of(Vector.empty[Action])
+      sourceAndAck <- LowLevelSource.toSourceAndAck(testLowLevelSource(refActions, testConfig))
+      processor = windowedProcessor(refActions, testConfig)
+      fiber <- sourceAndAck.stream(config, processor).compile.drain.start
+      _ <- IO.sleep(91.seconds)
+      _ <- fiber.cancel
+      result <- refActions.get
+    } yield result must beEqualTo(
+      Vector(
+        Action.ProcessorStartedWindow("1970-01-01T00:00:00Z"),
+        Action.ProcessorReceivedEvents("1970-01-01T00:00:00Z", List("1", "2")),
+        Action.ProcessorReceivedEvents("1970-01-01T00:00:11Z", List("3", "4")),
+        Action.ProcessorReachedEndOfWindow("1970-01-01T00:00:15Z"),
+        Action.Checkpointed(List("1", "2", "3", "4")),
+        Action.ProcessorStartedWindow("1970-01-01T00:00:22Z"),
+        Action.ProcessorReceivedEvents("1970-01-01T00:00:22Z", List("5", "6")),
+        Action.ProcessorReceivedEvents("1970-01-01T00:00:33Z", List("7", "8")),
+        Action.ProcessorReceivedEvents("1970-01-01T00:00:44Z", List("9", "10")),
+        Action.ProcessorReceivedEvents("1970-01-01T00:00:55Z", List("11", "12")),
+        Action.ProcessorReceivedEvents("1970-01-01T00:01:06Z", List("13", "14")),
+        Action.ProcessorReceivedEvents("1970-01-01T00:01:17Z", List("15", "16")),
+        Action.ProcessorReachedEndOfWindow("1970-01-01T00:01:22Z"),
+        Action.Checkpointed(List("5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16")),
+        Action.ProcessorStartedWindow("1970-01-01T00:01:28Z"),
+        Action.ProcessorReceivedEvents("1970-01-01T00:01:28Z", List("17", "18")),
+        Action.ProcessorReachedEndOfWindow("1970-01-01T00:01:31Z"),
+        Action.Checkpointed(List("17", "18"))
       )
     )
 