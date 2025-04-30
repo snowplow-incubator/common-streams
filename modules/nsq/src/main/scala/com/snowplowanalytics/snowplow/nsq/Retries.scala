a5c16799a2b8ea8202bb535ff9e1afb309067446
spenes <enes@snowplowanalytics.com>
2025-04-30 15:40:44 +0300

    Add NSQ module

---
+++ modules/nsq/src/main/scala/com/snowplowanalytics/snowplow/nsq/Retries.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023-present Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Snowplow Community License Version 1.0,
+ * and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
+ * You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
+ */
+package com.snowplowanalytics.snowplow.nsq
+
+import cats.Applicative
+import retry.{RetryPolicies, RetryPolicy}
+
+object Retries {
+
+  def fullJitter[F[_]: Applicative](config: BackoffPolicy): RetryPolicy[F] = {
+    val capped = RetryPolicies.capDelay[F](config.maxBackoff, RetryPolicies.fullJitter[F](config.minBackoff))
+    config.maxRetries.fold(capped)(max => capped.join(RetryPolicies.limitRetries(max)))
+  }
+
+}
---
+++ modules/nsq/src/main/scala/com/snowplowanalytics/snowplow/sources/nsq/NsqCheckpointer.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023-present Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Snowplow Community License Version 1.0,
+ * and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
+ * You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
+ */
+package com.snowplowanalytics.snowplow.sources.nsq
+
+import cats.implicits._
+import cats.effect.Async
+
+import com.sproutsocial.nsq.Message
+
+import com.snowplowanalytics.snowplow.sources.internal.Checkpointer
+
+class NsqCheckpointer[F[_]: Async] extends Checkpointer[F, Vector[Message]] {
+
+  override def combine(x: Vector[Message], y: Vector[Message]): Vector[Message] =
+    x |+| y
+
+  override def empty: Vector[Message] = Vector.empty
+
+  override def ack(messages: Vector[Message]): F[Unit] =
+    messages.traverse_(m => Async[F].delay(m.finish()))
+
+  override def nack(messages: Vector[Message]): F[Unit] =
+    messages.traverse_(m => Async[F].delay(m.requeue()))
+}
